6511330ab23fad0b142115b53b5567ca91c8f39b
zerochristof <christoffer@zarc.se>
2025-01-19 17:41:50 +0100

    Update index.ts

--- airbean-api/api/index.ts
+++ airbean-api/api/index.ts
@@ -1,7 +1,7 @@
-export {};
+
 const express = require("express");
 const app = express();
-
+export {};
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
